524b1bd90c18a9b0abd0d83d25c6a32e399f1d04
YuanGao0702 <gaoyuan0702@gwu.edu>
2019-03-27 01:06:00 -0400

    move folder

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-	
+
   state = {users: []}
 
   componentDidMount() {
-    fetch('/users')
+    fetch('/allusers')
       .then(res => res.json())
       .then(users => this.setState({ users }));
   }
-	
+
   render() {
     return (
       <div className="App">
